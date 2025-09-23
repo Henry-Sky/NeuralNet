1f19338d87a2deb537f77843c20da8073a3df8bc
Henry <henrysky64@outlook.com>
2025-09-23 16:03:12 +0800

    it just works

--- BPNET_numpy.py
+++ BPNET_numpy.py
@@ -1,4 +1,6 @@
 import os
+from tqdm import tqdm
+import logging
 from PIL import Image
 import numpy as np
 import warnings
@@ -9,13 +11,13 @@ config = {
     "train_path":"dogs-vs-cats/train",
     "test_path":"dogs-vs-cats/test1",
     "regular_size":(64, 64),
-    "hidden_layer_sizes":(32*32, 8*8),
-    "activation_funcs":("relu", "relu"),
+    "hidden_layer_sizes":(32*32, 16*16, 8*8),
+    "activation_funcs":("relu", "relu", "relu"),
     "output_func":"sigmoid",
     "loss_func":"binary_cross_entropy",
-    "batch_size":3,
-    "learning_rate":0.001,
-    "show_info":True,
+    "batch_size":24,
+    "learning_rate":0.01,
+    "show_info":False,
     "epsilon":1e-6,
 }
 
@@ -28,13 +30,13 @@ activate_funcs = {
 
 loss_funcs = {
     "binary_cross_entropy": lambda y_true, y_out: - np.mean(y_true * np.log(y_out) + (1 - y_true) * np.log(1 - y_out)),
-    "binary_cross_entropy_back": lambda y_true, y_out: (y_out - y_true) / y_true.size,
+    "binary_cross_entropy_back": lambda y_true, y_out: np.mean(y_out - y_true),
 }
 
-def min_max_normalize(data):
-    min_value = np.min(data)
-    max_value = np.max(data)
-    normalized_data = (data - min_value) / (max_value - min_value) + config["epsilon"]
+def min_max_normalize(origin_data):
+    min_value = np.min(origin_data)
+    max_value = np.max(origin_data)
+    normalized_data = (origin_data - min_value) / (max_value - min_value) + config["epsilon"]
     return normalized_data
 
 def regular_img(img):
@@ -59,8 +61,9 @@ def load_data(path):
             raise ValueError(f"Unexpected file name: {filename}")
     imagines = np.array(imagines)
     labels = np.array(labels)
-    data = {"datas": imagines, "labels": labels, "data_num":len(imagines), "label_num":len(labels)}
-    assert data["data_num"] == data["label_num"]
+    nums = len(imagines)
+    assert nums == len(labels)
+    data = {"datas": imagines, "labels": labels, "nums": nums}
 
     if config["show_info"]:
         print("-----data load-----")
@@ -95,22 +98,21 @@ def init_parameters():
 
 def batch_forward(batch_data, parameters):
     cache = {}
-    batch_size = config["batch_size"]
-    assert (batch_size == batch_data.shape[0])
     hidden_layer_sizes = config["hidden_layer_sizes"]
     hidden_layer_lens = len(hidden_layer_sizes)
-    (w, h) = config["regular_size"]
 
     for index in range(hidden_layer_lens + 1):
         if index == 0:
-            cache['z'+str(index)] = batch_data @ parameters['W'+str(index)] + parameters['b'+str(index)]
+            cache[f"A{index-1}"] = batch_data
+            cache[f"Z{index}"] = cache[f"A{index-1}"] @ parameters[f"W{index}"] + parameters[f"b{index}"]
         else:
-            cache['z'+str(index)] = cache['a'+str(index-1)] @ parameters['W'+str(index)] + parameters['b'+str(index)]
+            cache[f"Z{index}"] = cache[f"A{index-1}"] @ parameters[f"W{index}"] + parameters[f"b{index}"]
         if index == hidden_layer_lens:
-            cache['a' + str(index)] = activate_funcs[config["output_func"]](cache['z' + str(index)])
+            cache[f"A{index}"] = activate_funcs[config["output_func"]](cache[f"Z{index}"])
+            cache["Y"] = cache[f"A{index}"]
         else:
             func = config["activation_funcs"][index]
-            cache['a' + str(index)] = activate_funcs[func](cache['z' + str(index)])
+            cache[f"A{index}"] = activate_funcs[func](cache[f"Z{index}"])
 
     if config["show_info"]:
         print("-----forward cache-----")
@@ -119,41 +121,67 @@ def batch_forward(batch_data, parameters):
 
     return cache
 
-def batch_backward(batch_label, parameters, cache):
-    grad_parameters = {}
-    hidden_layers_num = len(config["hidden_layer_sizes"])
-    batch_label_pred = cache['a'+str(len(config["hidden_layer_sizes"])-1)]
-    loss = np.mean(loss_funcs[config["loss_func"]](batch_label_pred, batch_label))
-    loss_back = np.mean(loss_funcs[config["loss_func"]+"_back"](batch_label_pred, batch_label))
+def count_loss(y_true, y_out):
+    func = config["loss_func"]
+    return loss_funcs[func](y_true, y_out)
 
-    for index in range(hidden_layers_num - 1, -1, -1):
-        differ_func =activate_funcs[config["activation_funcs"][index] + "_back"]
-        if index == hidden_layers_num - 1:
-            grad_parameters["db"+str(index)] = loss_back * differ_func(cache['Y'+str(index)])
+def batch_backward(labels, parameters, cache):
+    grad_parameters = {}
+    hidden_layer_sizes = config["hidden_layer_sizes"]
+    hidden_layer_lens = len(hidden_layer_sizes)
+
+    for index in range(hidden_layer_lens , -1, -1):
+        if index == hidden_layer_lens:
+            func = config["output_func"]
+            loss_func = config["loss_func"]
+            differ_func = activate_funcs[f"{func}_back"]
+            grad_parameters[f"dA{index}"] = loss_funcs[f"{loss_func}_back"](labels, cache[f"A{index}"])
+            grad_parameters[f"dZ{index}"] = grad_parameters[f"dA{index}"] * differ_func(cache[f"Z{index}"])
+            grad_parameters[f"dW{index}"] = cache[f"A{index-1}"].T @ grad_parameters[f"dZ{index}"]
+            grad_parameters[f"db{index}"] = grad_parameters[f"dA{index}"]
         else:
-            grad_parameters["db"+str(index)] = loss_back * differ_func(cache['Y'+str(index)]) *\
-                                               np.outer(parameters['W'+str(index+1)], grad_parameters["db"+str(index+1)])
-        if index == 0:
-            continue
-        else:
-            grad_parameters["dW"+str(index)] = loss_back * np.outer(grad_parameters["db"+str(index)], cache['A'+str(index-1)])
+            func = config["activation_funcs"][index]
+            differ_func = activate_funcs[f"{func}_back"]
+            grad_parameters[f"dA{index}"] = (grad_parameters[f"dA{index+1}"] * grad_parameters[f"dZ{index+1}"]) @ parameters[f"W{index+1}"].T
+            grad_parameters[f"dZ{index}"] = grad_parameters[f"dA{index}"] * differ_func(cache[f"Z{index}"])
+            grad_parameters[f"dW{index}"] = cache[f"A{index-1}"].T @ grad_parameters[f"dZ{index}"]
+            grad_parameters[f"db{index}"] = grad_parameters[f"dA{index}"]
+
+    if config["show_info"]:
+        print("-----backward grad-----")
+        for key, value in grad_parameters.items():
+            if key[0:2] == "dW" or key[0:2] == "db":
+                print(f"{key}: {value.shape} -max:{np.max(value)} -min:{np.min(value)}")
+
     return grad_parameters
 
-def batch_update(data, parameters):
-    batch_size = config["batch_size"]
-    img_num = data["data_num"]
-    for batch_index in range(0, img_num, batch_size):
-        # 0, 3, 6
-        batch_x = data["datas"][batch_index:min(img_num, batch_index+batch_size)]
-        cache = batch_forward(batch_x, parameters)
-        batch_y = data["labels"][batch_index:min(img_num, batch_index+batch_size)]
-        grad_parameters = batch_backward(batch_y, parameters, cache)
-        for key in parameters.keys():
-            parameters[key] -= config["learning_rate"] * grad_parameters['d'+str(key)]
-    return parameters
+def param_update(param, grad):
+    learn_rate = config["learning_rate"]
+
+    for key, value in param.items():
+        value -= learn_rate  * grad[f"d{key}"]
+
+    return param
 
 if __name__ == "__main__":
-    data = load_data(config["train_path"])["datas"]
+    ld = load_data(config["train_path"])
+    data = ld["datas"]
+    label = ld["labels"]
+    nums = ld["nums"]
+    batch_size = config["batch_size"]
+
     param = init_parameters()
-    batch_forward(data[0:3], param)
+    for iter in tqdm(range(10)):
+        loss_list = []
+        for i in range(0, nums-batch_size, batch_size):
+            imgs = data[i:i+batch_size]
+            labs = label[i:i + batch_size]
+            cache = batch_forward(imgs, param)
+            loss = count_loss(labs, cache["Y"])
+            grad = batch_backward(labs, param, cache)
+            param = param_update(param, grad)
+            loss_list.append(loss)
+        avg_loss = np.sum(loss_list) / len(loss_list)
+        print(f"iter: {iter+1}, avg_loss: {avg_loss}")
+
 
